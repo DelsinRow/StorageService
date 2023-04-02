adbbdcd2f7133c0853dbf9c1cff87530483babaa
Evgenii Sinaev <evgenii.sinaev@orioninc.com>
2023-04-02 10:37:17 +0200

    New commit 1.Just update get-request

---
+++ src/main/java/com/orioinc/storageservice/TestRequest.java
@@ -0,0 +1,13 @@
+package com.orioinc.storageservice;
+
+public class TestRequest {
+    private String inputText;
+
+    public String getInputText() {
+        return inputText;
+    }
+
+    public void setInputText(String inputText) {
+        this.inputText = inputText;
+    }
+}
