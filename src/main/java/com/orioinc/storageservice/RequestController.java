79219372c856c1fc10f6fe7b23c2f9a1f082dd77
Evgenii Sinaev <evgenii.sinaev@orioninc.com>
2023-04-03 12:57:57 +0200

    New commit 1. Added encode method 2. Added POST-method

--- src/main/java/com/orioinc/storageservice/RequestController.java
+++ src/main/java/com/orioinc/storageservice/RequestController.java
@@ -4,31 +4,51 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("documents")
 public class RequestController {
 
-    @GetMapping("/test-request/{key}")
-    public TestRequest testRequest(@PathVariable String key) {
-        TestRequest testRequest = new TestRequest();
-        testRequest.setInputText("This text i get following key-link");
-        return testRequest;
+    private Map<String, String> map = new HashMap<>() {{
+        put("qwerty", "This is first row");
+        put("asdfgh", "This is second row");
+        put("zxcvbn", "This is third row");
+
+    }};
+
+    @GetMapping
+    public Map<String, String> map() {
+        return map;
     }
 
-    @PostMapping("/test-request")
-    public void createPostRequest(@RequestBody TestRequest postRequest) {
-        //create mapping my request
+    @GetMapping("{key}")
+    public String getText(@PathVariable String key) {
+        return map.get(key);
     }
 
-    public void sendRequest() {
-        RestTemplate restTemplate = new RestTemplate();
+    @PostMapping
+    public String create(@RequestBody String text) {
+        String newKey = new EncodeClass().encodeKey(text);
 
-        TestRequest testRequest = restTemplate.getForObject("http://localhost:8080/test-request/first-key", TestRequest.class);
+        map.put(newKey, text);
 
-//        TestRequest newTestRequest = new TestRequest();
-//        newTestRequest.setInputText("Some text for checking post request");
-//        restTemplate.postForObject("http://localhost:8080/test-request", newTestRequest, Void.class);
+        return text;
     }
+
+    //send object in DB
+    //create hash-key
+    //set key-field by dataText object
+    //create .../hash - link
+//    public void sendRequest() {
+//        RestTemplate restTemplate = new RestTemplate();
+//        DataText dataText = restTemplate.getForObject("http://localhost:8080/document/key2 ", DataText.class);
+//        DataText dataText = new DataText();
+//        dataText.setInputText("Some text for checking post request");
+//        restTemplate.postForObject("http://localhost:8080/document", newTestRequest, Void.class);
+//    }
 }
