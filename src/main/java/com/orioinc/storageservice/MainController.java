6d913d70b0253c6e9cf8f5df429b06a142434bdc
Evgenii Sinaev <evgenii.sinaev@orioninc.com>
2023-04-05 17:37:37 +0200

    New commit - correct deploy on local server

--- src/main/java/com/orioinc/storageservice/MainController.java
+++ src/main/java/com/orioinc/storageservice/MainController.java
@@ -3,13 +3,13 @@ package com.orioinc.storageservice;
 import com.orioinc.storageservice.model.DataText;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @Slf4j
 @AllArgsConstructor
@@ -20,9 +20,11 @@ public class MainController {
     private final CreateUniqueKeyService CreateUniqueKeyService;
     private final Repository repository;
 
-    @GetMapping("/get/{uniqueKey}")
+    private final MongoTemplate mongoTemplate;
+
+    @GetMapping("/get/{key}")
     public String getText(@PathVariable String key) {
-//        log.debug("invoke getText {}", uniqueKey);
+        //add exception handling
         return repository.findByKey(key).getInputText();
     }
 
