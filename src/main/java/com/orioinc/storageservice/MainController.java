c3ff224f775fb0ef4d128018ed6acacdf7e5c259
Evgenii Sinaev <evgenii.sinaev@orioninc.com>
2023-04-06 13:20:02 +0200

    New commit - added handling exception

--- src/main/java/com/orioinc/storageservice/MainController.java
+++ src/main/java/com/orioinc/storageservice/MainController.java
@@ -1,14 +1,20 @@
 package com.orioinc.storageservice;
 
+import com.orioinc.storageservice.exceptions.ResponseError;
 import com.orioinc.storageservice.model.DataText;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -19,13 +25,13 @@ public class MainController {
 
     private final CreateUniqueKeyService CreateUniqueKeyService;
     private final Repository repository;
+    private final HttpServletResponse response;
 
     private final MongoTemplate mongoTemplate;
 
     @GetMapping("/get/{key}")
     public String getText(@PathVariable String key) {
-        //add exception handling
-        return repository.findByKey(key).getInputText();
+            return repository.findByKey(key).getInputText();
     }
 
     @PostMapping("/post")
@@ -36,9 +42,17 @@ public class MainController {
         return "Your key: " + data.getKey();
     }
 
-//    @ExceptionHandler(IllegalArgumentException.class)
-//    public String handle(IllegalArgumentException e) {
-//        log.error(e.getMessage());
-//        return "Something wrong";
-//    }
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseError notFoundKey(NullPointerException exception) {
+        log.error(exception.getMessage(), exception);
+        return new ResponseError("Invalid key", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseError emptyRequest(HttpMessageNotReadableException exception) {
+        log.error(exception.getMessage(), exception);
+        return new ResponseError("Empty request. Please, enter some text  ", HttpStatus.BAD_REQUEST);
+    }
 }
